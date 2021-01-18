661c88ef6cc9b82b5967d5ed64b4752dfa8864b1
Souvikdeb2612 <souvikdeb26@gmail.com>
2021-01-18 23:14:23 +0530

    Final update

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var selectedValue3 = document.getElementById("pro3");
 var selectedValue4 = document.getElementById("pro4");
 var selectedValue5 = document.getElementById("pro5");
 var selectedValue6 = document.getElementById("pro6");
-var id1=document.getElementById("hh");
+
+
 function register(){
     x.style.left="-450px";
     y.style.left="50px";
@@ -20,24 +21,45 @@ function login(){
 
 }
 
-function getSelectValue1(){
-    id1.value = selectedValue1.value
+function dashboardLoad() {
+    getSelectValue1();
+    getSelectValue2();
+    getSelectValue3();
+    getSelectValue4();
+    getSelectValue5();
+    getSelectValue6();
 }
 
-function getSelectValue2(){
-    return selectedValue2.value
-}
-function getSelectValue3(){
-    return selectedValue3.value
-}
-function getSelectValue4(){
-    return selectedValue4.value
-}
-function getSelectValue5(){
-    return selectedValue5.value
-}
-function getSelectValue6(){
-    return selectedValue6.value
+
+function getSelectValue1() {
+  document.getElementById('value1').innerText = getQueryVariable('pro1');
 }
 
-getSelectValue1()
+function getSelectValue2() {
+  document.getElementById('value2').innerText = getQueryVariable('pro2');
+}
+function getSelectValue3() {
+  document.getElementById('value3').innerText = getQueryVariable('pro3');
+}
+function getSelectValue4() {
+  document.getElementById('value4').innerText = getQueryVariable('pro4');
+}
+function getSelectValue5() {
+  document.getElementById('value5').innerText = getQueryVariable('pro5');
+}
+function getSelectValue6() {
+  document.getElementById('value6').innerText = getQueryVariable('pro6');
+}
+
+
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    if (decodeURIComponent(pair[0]) == variable) {
+      return decodeURIComponent(pair[1]);
+    }
+  }
+  console.log('Query variable %s not found', variable);
+}
